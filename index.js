cfc49d4464c8880e520de8126095a772a3587572
Mkyla <kylamaem23@gmail.com>
2024-05-25 11:39:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const btnLike1= document.getElementById("btnLike1")
+const btnLike2= document.getElementById("btnnLike2")
+const countLikes = document.getElementById("countLikes1")
+const countLikes = document.getElementById("countLikes2")
 
+function hitLikes1() {
+ let totalLikes1 = parseInt(countLikes1) + 1
+  countLikes1.textcontent = totalLikes1.toString()
+}
+btnLike1.atEventListener("click",hitLikes1)
